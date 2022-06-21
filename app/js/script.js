f36f6c4fd39f39f1960ae55d88fe2b396cdc9a0d
Peter Emad <peteremad95@gmail.com>
2022-06-21 21:12:22 +0200

    adding some styles

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,6 @@
 import Storage from "./modules/Storage.js";
 import List from "./modules/List.js";
+import Sortable from "../../node_modules/sortablejs/modular/sortable.esm.js";
 // import Sortable from "sortablejs";
 /* ------------------------------- Add list btn ------------------------------- */
 const addListBtn = document.querySelector(".add_list_btn");
@@ -53,30 +54,50 @@ updateUI();
 
 let sortablelists = document.querySelectorAll(".tasks");
 sortablelists.forEach((list) => {
-	Sortable.create(list, {
+	let sortable = Sortable.create(list, {
 		group: {
-			name: "lists",
+			name: "sortableLists",
 			// pull: "clone",
 			// revertClone: true,
 		},
+
 		// sort: true, // sorting inside list
 		// delay: 100, // time in milliseconds to define when the sorting should start
 		// delayOnTouchOnly: false, // only delay if user is using touch
 		// touchStartThreshold: 0, // px, how many pixels the point should move before cancelling a delayed drag event
 		// disabled: true, // Disables the sortable if set to true.
-		// store: null, // @see Store
+		// store: {
+		// 	/**
+		// 	 * Get the order of elements. Called once during initialization.
+		// 	 * @param   {Sortable}  sortable
+		// 	 * @returns {Array}
+		// 	 */
+		// 	get: function (sortable) {
+		// 		var order = localStorage.getItem(sortable.options.group.name);
+		// 		return order ? order.split("|") : [];
+		// 	},
+
+		// 	/**
+		// 	 * Save the order of elements. Called onEnd (when the item is dropped).
+		// 	 * @param {Sortable}  sortable
+		// 	 */
+		// 	set: function (sortable) {
+		// 		var order = sortable.toArray();
+		// 		localStorage.setItem(sortable.options.group.name, order.join("|"));
+		// 	},
+		// }, // @see Store
 		animation: 150, // ms, animation speed moving items when sorting, `0` — without animation
 		// easing: "cubic-bezier(1, 0, 0, 1)", // Easing for animation. Defaults to null. See https://easings.net/ for examples.
-		// handle: ".handle", // Drag handle selector within list items
+		handle: ".handle", // Drag handle selector within list items
 		// filter: ".remove_task_btn", // Selectors that do not lead to dragging (String or Function)
 		// preventOnFilter: false, // Call `event.preventDefault()` when triggered `filter`
 		// draggable: ".task", // Specifies which items inside the element should be draggable
 
-		// dataIdAttr: "data-id", // HTML attribute that is used by the `toArray()` method
+		dataIdAttr: "data-id", // HTML attribute that is used by the `toArray()` method
 
-		// ghostClass: "ghost", // Class name for the drop placeholder
-		// chosenClass: "chosen", // Class name for the chosen item
-		// dragClass: "drag", // Class name for the dragging item
+		ghostClass: "ghost", // Class name for the drop placeholder
+		chosenClass: "chosen", // Class name for the chosen item
+		dragClass: "drag", // Class name for the dragging item
 
 		// swapThreshold: 1, // Threshold of the swap zone
 		// invertSwap: false, // Will always use inverted swap zone if set to true
@@ -93,4 +114,6 @@ sortablelists.forEach((list) => {
 		// removeCloneOnHide: true, // Remove the clone element when it is not showing, rather than just hiding it
 		// emptyInsertThreshold: 5, // px, distance mouse must be from empty sortable to insert drag element into it
 	});
+	// var order = sortable.toArray();
+	// sortable.sort(order.reverse(), true); // apply
 });
